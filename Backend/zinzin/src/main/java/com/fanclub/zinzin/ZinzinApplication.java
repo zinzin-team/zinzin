db9b7d0d4ce30decef2366949dbd5fe4b1f45d3f
조현아 <monicajo074@gmail.com>
2024-07-24 11:35:55 +0900

    Init: [BE] 프로젝트 생성

---
+++ Backend/zinzin/src/main/java/com/fanclub/zinzin/ZinzinApplication.java
@@ -0,0 +1,13 @@
+package com.fanclub.zinzin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZinzinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZinzinApplication.class, args);
+	}
+
+}
