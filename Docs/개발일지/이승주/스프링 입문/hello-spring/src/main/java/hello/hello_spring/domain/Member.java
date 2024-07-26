3ef327b2c9f162c466eff761843ce501c676e020
Seungju Lee <crystal_lsj@naver.com>
2024-07-26 16:22:50 +0900

    Docs: 카드 생성 +

--- Docs/개발일지/이승주/스프링 입문/hello-spring/src/main/java/hello/hello_spring/domain/Member.java
+++ Docs/개발일지/이승주/스프링 입문/hello-spring/src/main/java/hello/hello_spring/domain/Member.java
@@ -1,8 +1,16 @@
 package hello.hello_spring.domain;
 
-public class Member {
+// JPA를 사용하려면 entity 매핑 필요
+// jpa는 표준 인터페이스, 구현은 따로
+// ORM(Object Relational Mmapping)
+import jakarta.persistence.*;
 
+@Entity
+public class Member {
+    // id 매핑 + 알아서 자동으로 생성
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+//    @Column(name = "name")
     private String name;
 
     public Long getId() {
