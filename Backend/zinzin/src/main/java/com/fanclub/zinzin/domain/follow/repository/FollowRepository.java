60972db5d34182fbb4c59e2f108c1209eb1b0cfe
조현아 <monicajo074@gmail.com>
2024-07-30 12:10:46 +0900

    Feat: [BE] 지인 요청 기능 변경

--- Backend/zinzin/src/main/java/com/fanclub/zinzin/domain/follow/repository/FollowRepository.java
+++ Backend/zinzin/src/main/java/com/fanclub/zinzin/domain/follow/repository/FollowRepository.java
@@ -5,11 +5,15 @@ import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.neo4j.repository.query.Query;
 
 public interface FollowRepository extends Neo4jRepository<Person, Long> {
-    @Query("MATCH (a:Person {member_id: $userMemberId})" +
+
+    @Query("MATCH (a:Person {member_id: $userMemberId}) " +
             "MATCH (b:Person {member_id: $targetMemberId}) " +
-            "OPTIONAL MATCH (a)-[r:FOLLOW|REQUEST_FOLLOW|REJECT_FOLLOW|UNFOLLOW]-(b) " +
-            "WITH a, b, r WHERE r IS NULL " +
-            "MERGE (a)-[:REQUEST_FOLLOW]->(b)" +
+            "OPTIONAL MATCH (a)-[r:REJECT_FOLLOW|UNFOLLOW]-(b) " +
+            "DELETE r " +
+            "WITH a, b " +
+            "OPTIONAL MATCH (a)-[r2:FOLLOW|REQUEST_FOLLOW]-(b) " +
+            "WITH a, b, r2 WHERE r2 IS NULL " +
+            "MERGE (a)-[:REQUEST_FOLLOW]->(b) " +
             "RETURN count(*) AS created")
     Integer createFollowRelation(Long userMemberId, Long targetMemberId);
 
