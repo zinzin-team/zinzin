48329745595ed12156d2881950adc1fe48794dce
조현아 <monicajo074@gmail.com>
2024-08-01 15:57:38 +0900

    Style: [BE] 사용하지 않는 코드 정리

--- Backend/zinzin/src/main/java/com/fanclub/zinzin/domain/member/controller/MemberController.java
+++ Backend/zinzin/src/main/java/com/fanclub/zinzin/domain/member/controller/MemberController.java
@@ -6,8 +6,6 @@ import com.fanclub.zinzin.domain.member.dto.MemberRegisterDto;
 import com.fanclub.zinzin.domain.member.service.MemberService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class MemberController {
 
-    private static final Logger log = LoggerFactory.getLogger(MemberController.class);
     private final MemberService memberService;
 
     @PostMapping("/register")
