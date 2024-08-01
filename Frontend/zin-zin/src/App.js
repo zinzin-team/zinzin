cd4fbf107b48fc0df361ba4f0fe9e4106c386cff
박상용 <psyok7@naver.com>
2024-08-01 09:50:59 +0900

    Feat: [FE] 매칭 화면 UI 구현

--- Frontend/zin-zin/src/App.js
+++ Frontend/zin-zin/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { useEffect } from "react";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import HomeView from "./views/HomeView";
 import ChatView from "./components/Chat";
 import FriendsView from "./components/Friends";
@@ -10,27 +9,43 @@ import MatchingView from "./components/Matching";
 import MypageView from "./components/Mypage";
 import SignupView from "./components/Signup";
 import Navbar from "./components/Navbar"; 
+import CreatecardView from './components/Createcard';
+import UpdatecardView from './components/Updatecard';
+import Header from './components/Header';
 import './App.css';
 
+const App = () => {
+  return (
+    <BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
+  );
+}
 
+const AppContent = () => {
+  const location = useLocation();
 
-function App() {
+  // 특정 경로에서 Navbar를 숨기기
+  const hideHeaderPaths = ['/friend'];
+  const hideNavbarPaths = ['/friend'];
+
   return (
     <div className="App"> 
-    <BrowserRouter>
-    <Routes>
+      {!hideHeaderPaths.includes(location.pathname) && <Header />}
+      <Routes>
         <Route path="/" element={<HomeView />} />
         <Route path="/chat" element={<ChatView />} />
-        <Route path="/friend" element={<FriendsView />} />
         <Route path="/leave" element={<LeaveView />} />
+        <Route path="/friend" element={<FriendsView />} />
         <Route path="/like" element={<LikeView />} />
         <Route path="/match" element={<MatchingView />} />
         <Route path="/mypage" element={<MypageView />} />
-        <Route path="/signup" element={<SignupView/>} />
-    </Routes>
-    <Navbar/>
-  </BrowserRouter>
-  </div>
+        <Route path="/signup" element={<SignupView />} />
+        <Route path="/create-card" element={<CreatecardView />} />
+        <Route path="/update-card" element={<UpdatecardView />} />
+      </Routes>
+      {!hideNavbarPaths.includes(location.pathname) && <Navbar />}
+    </div>
   );
 }
 
