86192fbae1903fc08b766b9f9e03b586892ff26f
YoonjiKim <jiwabin01@ajou.ac.kr>
2024-08-01 15:22:24 +0900

    Feat: [FE] 마이페이지 UI 구현 - MyProfile, MyCard, MypageList 컴포넌트 구분 - Mypage.module.css 생성 - Myprofile 경로 변경

--- Frontend/zin-zin/src/components/mypage/Mypage.jsx
+++ Frontend/zin-zin/src/components/mypage/Mypage.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-
+import MyProfile from './MyProfile';
+import MyCard from './Mycard';
+import MypageList from './MypageList';
+import styles from './Mypage.module.css';
+  
 const Mypage = () => {
-    return (
-        <div>
-            마이페이지 탭
-        </div>
-    )
+  return (
+    <div className={styles.mypageContainer}>
+      <MyProfile />
+      <MyCard />
+      <MypageList />
+    </div>
+  )
 }
 
-export default Mypage
+export default Mypage;
