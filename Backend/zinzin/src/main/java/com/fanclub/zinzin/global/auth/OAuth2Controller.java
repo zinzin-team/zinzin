cef526168e5ad699d4e97bdcbfc0dde64e432ee2
ParkSangwoo <psw1253@naver.com>
2024-08-02 15:15:45 +0900

    Feat: [BE] 회원가입 시 친구 목록 불러와서 db에 캐싱하는 로직 추가. 회원가입 후 데이터 삭제 처리 해야함.

--- Backend/zinzin/src/main/java/com/fanclub/zinzin/global/auth/OAuth2Controller.java
+++ Backend/zinzin/src/main/java/com/fanclub/zinzin/global/auth/OAuth2Controller.java
@@ -1,7 +1,6 @@
 package com.fanclub.zinzin.global.auth;
 
 import com.fanclub.zinzin.domain.member.entity.Member;
-import com.fanclub.zinzin.domain.member.entity.Role;
 import com.fanclub.zinzin.domain.member.repository.MemberRepository;
 import com.fanclub.zinzin.global.auth.dto.MemberAuthResponseDto;
 import com.fanclub.zinzin.global.util.JwtUtil;
@@ -13,8 +12,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/oauth2")
@@ -33,23 +30,13 @@ public class OAuth2Controller {
     @GetMapping("/kakao/callback")
     public ResponseEntity<MemberAuthResponseDto> kakaoCallback(@RequestParam("code") String code) {
         ArrayList<String> tokens = oAuth2Service.getKakaoTokens(code);
+        String kakaoAccessToken = tokens.get(0);
         String idToken = tokens.get(2);
 
         String[] claims = jwtUtil.getSubAndEmailFromIdToken(idToken);
         String sub = claims[0];
-        String email = claims[1];
 
         Member member = memberRepository.findBySub(sub);
-
-        if (member != null) {
-            Long memberId = member.getId();
-            Map<String, String> tokensMap = oAuth2Service.generateTokens(memberId, sub, Role.USER);
-            String accessToken = tokensMap.get("accessToken");
-            String refreshToken = tokensMap.get("refreshToken");
-
-            return ResponseEntity.ok(MemberAuthResponseDto.createTokenResponse(accessToken, refreshToken));
-        }
-        MemberAuthResponseDto responseDto = MemberAuthResponseDto.createRegisterResponse(sub, email);
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseDto);
+        return ResponseEntity.ok(oAuth2Service.loginOrRegister(member, claims, kakaoAccessToken));
     }
 }
---
+++ Backend/zinzin/src/main/java/com/fanclub/zinzin/global/auth/repository/TempFriendRepository.java
@@ -0,0 +1,7 @@
+package com.fanclub.zinzin.global.auth.repository;
+
+import com.fanclub.zinzin.global.auth.entity.TempFriend;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TempFriendRepository extends JpaRepository<TempFriend, Long> {
+}
