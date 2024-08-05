60199e7bb14f5769b88c4122d735c59fff068339
YoonjiKim <jiwabin01@ajou.ac.kr>
2024-08-05 10:37:26 +0900

    Feat: [FE] Signup css 수정 로그인 api 연결 (미완)

--- Frontend/zin-zin/src/App.js
+++ Frontend/zin-zin/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import LoginPage from "./components/signup/LoginPage";
+import CallbackPage from "./components/signup/CallbackPage";
+import SignupView from "./views/SignupView";
 import HomeView from "./views/HomeView";
 import ChatView from "./components/chating/Chat";
 import FriendsView from "./components/friends/Friends";
@@ -7,7 +10,6 @@ import LeaveView from "./components/signup/Leave";
 import LikeView from "./components/list/Like";
 import MatchingView from "./components/matching/Matching";
 import MypageView from "./components/mypage/Mypage";
-import SignupView from "./components/signup/Signup";
 import Navbar from "./components/navbar/Navbar"; 
 import CreatecardView from './components/matching/Createcard';
 import UpdatecardView from './components/mypage/Updatecard';
@@ -25,26 +27,31 @@ const App = () => {
 const AppContent = () => {
   const location = useLocation();
 
-  // 특정 경로에서 Navbar를 숨기기
-  const hideHeaderPaths = ['/friend'];
-  const hideNavbarPaths = ['/friend'];
+  // 특정 경로에서 Header와 Navbar를 숨기기
+  const hideHeaderPaths = ['/friend', '/login', '/signup'];
+  const hideNavbarPaths = ['/friend', '/login', '/signup'];
+
+  const isHeaderHidden = hideHeaderPaths.some(path => location.pathname.startsWith(path));
+  const isNavbarHidden = hideNavbarPaths.some(path => location.pathname.startsWith(path));
 
   return (
-    <div className="App"> 
-      {!hideHeaderPaths.includes(location.pathname) && <Header />}
+    <div className="App">
+      {!isHeaderHidden && <Header />}
       <Routes>
         <Route path="/" element={<HomeView />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/api/oauth2/kakao/callback" element={<CallbackPage />} />
+        <Route path="/signup/*" element={<SignupView />} />
         <Route path="/chat" element={<ChatView />} />
         <Route path="/leave" element={<LeaveView />} />
         <Route path="/friend" element={<FriendsView />} />
         <Route path="/like" element={<LikeView />} />
         <Route path="/match" element={<MatchingView />} />
         <Route path="/mypage" element={<MypageView />} />
-        <Route path="/signup" element={<SignupView />} />
         <Route path="/create-card" element={<CreatecardView />} />
         <Route path="/update-card" element={<UpdatecardView />} />
       </Routes>
-      {!hideNavbarPaths.includes(location.pathname) && <Navbar />}
+      {!isNavbarHidden && <Navbar />}
     </div>
   );
 }
