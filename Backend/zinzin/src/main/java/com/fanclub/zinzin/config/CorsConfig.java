924046ca4f69341a8b1ad562559cb7e8bfe9b37f
조현아 <monicajo074@gmail.com>
2024-08-06 11:08:36 +0900

    Fix: [BE] CorsConfig allowedOriginPatterns로 변경

--- Backend/zinzin/src/main/java/com/fanclub/zinzin/config/CorsConfig.java
+++ Backend/zinzin/src/main/java/com/fanclub/zinzin/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // 리액트 앱이 실행되는 도메인 및 포트
+                .allowedOriginPatterns("*") // 리액트 앱이 실행되는 도메인 및 포트
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
