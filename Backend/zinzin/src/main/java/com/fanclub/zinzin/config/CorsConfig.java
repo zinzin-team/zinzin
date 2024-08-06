a73340a73f5dd6bb34050c84972a2a93b916b96c
홍창기 <hckhong@naver.com>
2024-08-06 10:52:04 +0900

    Fix: [BE] CorsConfig 패키지 변경

--- Backend/zinzin/src/main/java/com/fanclub/zinzin/config/CorsConfig.java
+++ Backend/zinzin/src/main/java/com/fanclub/zinzin/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.fanclub.zinzin.global.config;
+package com.fanclub.zinzin.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
