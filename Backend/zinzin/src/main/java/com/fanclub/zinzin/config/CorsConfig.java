25087a8ab1bcaeae8191db880c54813abe87ea7d
조현아 <monicajo074@gmail.com>
2024-08-06 12:07:18 +0900

    Fix: [BE] 돌고돌아 원상복구인가

--- Backend/zinzin/src/main/java/com/fanclub/zinzin/config/CorsConfig.java
+++ Backend/zinzin/src/main/java/com/fanclub/zinzin/config/CorsConfig.java
@@ -1,24 +1,16 @@
 package com.fanclub.zinzin.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")  // 모든 경로에 대해 CORS를 허용
-                        .allowedOrigins("http://localhost:3000")  // 허용할 도메인
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")  // 허용할 HTTP 메서드
-                        .allowedHeaders("*")  // 허용할 헤더
-                        .allowCredentials(true);  // 자격 증명 사용 허용
-            }
-        };
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .maxAge(3000);
     }
 }
