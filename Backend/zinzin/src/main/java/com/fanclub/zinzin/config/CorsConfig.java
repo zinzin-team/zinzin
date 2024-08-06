ab8c1cf930c1526fb4d97cf909c3508f6ecf53bf
조현아 <monicajo074@gmail.com>
2024-08-06 12:35:26 +0900

    Fix: [BE] 아몰랑

--- Backend/zinzin/src/main/java/com/fanclub/zinzin/config/CorsConfig.java
+++ Backend/zinzin/src/main/java/com/fanclub/zinzin/config/CorsConfig.java
@@ -4,14 +4,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000","https://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOriginPatterns("*")
                 .allowCredentials(true)
-                .maxAge(3000);
+                .maxAge(3600)
+                .allowedHeaders("*")
+                .allowedMethods("*");
+
     }
 }
