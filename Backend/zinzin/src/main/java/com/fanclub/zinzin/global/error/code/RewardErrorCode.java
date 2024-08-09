eab11309eb6dcd0e456171193b4fd6ac0cfc8786
Seungju Lee <crystal_lsj@naver.com>
2024-08-09 10:57:06 +0900

    Feat: [BE] 채팅방 DB와 연결, 관련 ErrorCode 및 예외 처리 추가

--- Backend/zinzin/src/main/java/com/fanclub/zinzin/global/error/code/RewardErrorCode.java
+++ Backend/zinzin/src/main/java/com/fanclub/zinzin/global/error/code/RewardErrorCode.java
@@ -8,9 +8,10 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum RewardErrorCode implements ErrorCode {
 
-    ALREADY_SELECTED(HttpStatus.BAD_REQUEST, "R001", "이미 주선자를 선택하였습니다."),
-    DUPLICATED(HttpStatus.BAD_REQUEST, "R002", "해당 채팅방의 구성원이 아니거나 중복된 데이터가 들어갔습니다. DB 확인이 필요합니다."),
-    SELF_SELECTED(HttpStatus.BAD_REQUEST, "R003", "본인을 선택할 수 없습니다.");
+    NOT_MEMBER_OF_CHATROOM(HttpStatus.FORBIDDEN, "R001", "해당 채팅방의 구성원이 아닙니다."),
+    ALREADY_SELECTED(HttpStatus.BAD_REQUEST, "R002", "이미 주선자를 선택하였습니다."),
+    DUPLICATED(HttpStatus.BAD_REQUEST, "R003", "중복된 데이터가 들어갔습니다. DB 확인이 필요합니다."),
+    SELF_SELECTED(HttpStatus.BAD_REQUEST, "R004", "본인을 선택할 수 없습니다.");
 
     private final HttpStatus status;
     private final String code;
