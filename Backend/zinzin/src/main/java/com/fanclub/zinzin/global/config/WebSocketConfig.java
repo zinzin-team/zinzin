354ccb4055a06af87d26577d67cbd3ffb4515503
홍창기 <hckhong@naver.com>
2024-08-10 12:26:22 +0900

    Fix: [BE] Endpoint 수정 취소

--- Backend/zinzin/src/main/java/com/fanclub/zinzin/global/config/WebSocketConfig.java
+++ Backend/zinzin/src/main/java/com/fanclub/zinzin/global/config/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/api/ws")
+        registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*");
     }
 
