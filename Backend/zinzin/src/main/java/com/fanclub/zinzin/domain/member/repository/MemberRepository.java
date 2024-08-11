dbf04f4a06783ac3dc3df4ffdfe61945af5fdb29
박상우 <psw1253@naver.com>
2024-08-11 19:49:32 +0900

    Feature: [BE] 로그인 후 리프레시토큰을 쿠키로 전달하게 변경

--- Backend/zinzin/src/main/java/com/fanclub/zinzin/domain/member/repository/MemberRepository.java
+++ Backend/zinzin/src/main/java/com/fanclub/zinzin/domain/member/repository/MemberRepository.java
@@ -1,10 +1,17 @@
 package com.fanclub.zinzin.domain.member.repository;
 
 import com.fanclub.zinzin.domain.member.entity.Member;
+import com.fanclub.zinzin.domain.member.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Member findByEmail(String email);
     Member findBySub(String sub);
+
+    @Query("SELECT m.role FROM Member m WHERE m.id = :memberId")
+    Role findRoleByMemberId(@Param("memberId") Long memberId);
+
 }
