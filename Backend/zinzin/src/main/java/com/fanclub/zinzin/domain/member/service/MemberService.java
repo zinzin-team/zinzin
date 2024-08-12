285d6c5171f66473874317dc831ba6b5f72424fe
ParkSangwoo <psw1253@naver.com>
2024-08-12 17:32:59 +0900

    Feature: [BE] 회원가입 토큰 발급 추가

--- Backend/zinzin/src/main/java/com/fanclub/zinzin/domain/member/service/MemberService.java
+++ Backend/zinzin/src/main/java/com/fanclub/zinzin/domain/member/service/MemberService.java
@@ -5,26 +5,29 @@ import com.fanclub.zinzin.domain.card.repository.CardRepository;
 import com.fanclub.zinzin.domain.friend.entity.TempFriend;
 import com.fanclub.zinzin.domain.friend.repository.TempFriendRepository;
 import com.fanclub.zinzin.domain.member.dto.*;
-import com.fanclub.zinzin.domain.member.entity.MatchingVisibility;
-import com.fanclub.zinzin.domain.member.entity.Member;
-import com.fanclub.zinzin.domain.member.entity.MemberInfo;
-import com.fanclub.zinzin.domain.member.entity.RandomNickname;
+import com.fanclub.zinzin.domain.member.entity.*;
 import com.fanclub.zinzin.domain.member.repository.MemberInfoRepository;
 import com.fanclub.zinzin.domain.member.repository.MemberRepository;
 import com.fanclub.zinzin.domain.member.repository.RandomNicknameRepository;
 import com.fanclub.zinzin.domain.person.entity.Person;
 import com.fanclub.zinzin.domain.person.repository.PersonRepository;
+import com.fanclub.zinzin.global.auth.OAuth2Service;
+import com.fanclub.zinzin.global.auth.dto.MemberAuthResponseDto;
 import com.fanclub.zinzin.global.error.code.CommonErrorCode;
 import com.fanclub.zinzin.global.error.code.MemberErrorCode;
 import com.fanclub.zinzin.global.error.exception.BaseException;
 import com.fanclub.zinzin.global.s3.S3Service;
+import com.fanclub.zinzin.global.util.JwtUtil;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 @Service
@@ -38,13 +41,14 @@ public class MemberService {
     private final CardRepository cardRepository;
     private final TempFriendRepository tempFriendRepository;
     private final S3Service s3Service;
+    private final OAuth2Service oAuth2Service;
+    private final JwtUtil jwtUtil;
 
     @Value("${random-nickname.size}")
     private int randomNicknameSize;
 
     @Transactional
-    public void registerNewMember(MemberRegisterDto memberRegisterDto) {
-
+    public MemberAuthResponseDto registerNewMember(HttpServletResponse response, MemberRegisterDto memberRegisterDto) {
         try {
             Member member = memberRegisterDto.toMemberEntity();
             memberRepository.save(member);
@@ -60,9 +64,23 @@ public class MemberService {
             }
 
             tempFriendRepository.deleteAll(tempFriends);
+
+            return generateRegisterTokens(response, member);
         } catch (Exception e) {
             throw new BaseException(MemberErrorCode.MEMBER_REGIST_FAILED);
         }
+    }
+
+    private MemberAuthResponseDto generateRegisterTokens(HttpServletResponse response, Member member) {
+        if (member != null) {
+            Long memberId = member.getId();
+            Map<String, String> tokensMap = oAuth2Service.generateTokens(memberId, member.getSub(), Role.USER);
+            String accessToken = tokensMap.get("accessToken");
+            jwtUtil.addRefreshTokenToCookie(response, tokensMap.get("refreshToken"));
+
+            return MemberAuthResponseDto.createTokenResponse(accessToken);
+        }
+        throw new BaseException (MemberErrorCode.MEMBER_REGIST_FAILED);
     }
 
     public CheckSearchIdResponse checkDuplicatedSearchId(String searchId) {
