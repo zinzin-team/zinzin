e665ceaa8347943f001a10ea2f0e47c1812bb873
ParkSangwoo <psw1253@naver.com>
2024-08-12 14:07:11 +0900

    Feature: [BE] 토큰 재발급 로직 분리

---
+++ Backend/zinzin/src/main/java/com/fanclub/zinzin/global/auth/controller/TokenController.java
@@ -0,0 +1,22 @@
+package com.fanclub.zinzin.global.auth.controller;
+
+import com.fanclub.zinzin.global.auth.dto.TokenDto;
+import com.fanclub.zinzin.global.auth.service.TokenService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class TokenController {
+
+    private final TokenService tokenService;
+
+    @GetMapping("/refresh")
+    public ResponseEntity<TokenDto> reGenerateToken(HttpServletRequest request, HttpServletResponse response) {
+        return ResponseEntity.ok(tokenService.refreshTokens(request, response));
+    }
+}
