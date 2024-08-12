f1e9144a863d6809d7cf36cf0e73843acbae52b7
조현아 <monicajo074@gmail.com>
2024-08-12 13:07:46 +0900

    Fix: [BE] memberInfo fk로 사용한 에러 해결

--- Backend/zinzin/src/main/java/com/fanclub/zinzin/domain/chatting/repository/ChatRoomRepository.java
+++ Backend/zinzin/src/main/java/com/fanclub/zinzin/domain/chatting/repository/ChatRoomRepository.java
@@ -7,20 +7,18 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
-    @Query("SELECT cr FROM ChatRoom cr JOIN cr.members m WHERE m.memberInfo.member.id = :memberId AND cr.status = :status")
+    @Query("SELECT cr FROM ChatRoom cr JOIN cr.members m WHERE m.member.id = :memberId AND cr.status = :status")
     List<ChatRoom> findAllByMemberIdAndStatus(Long memberId, ChatRoomStatus status);
 
     @Query("SELECT r FROM ChatRoom r " +
             "JOIN r.members m1 " +
             "JOIN r.members m2 " +
-            "WHERE m1.memberInfo.member.id = :myMemberId " +
-            "AND m2.memberInfo.member.id = :otherMemberId " +
-            "AND r.status = :status " +
-            "AND (SELECT COUNT(m) FROM ChatRoomMember m WHERE m.chatRoom.id = r.id) = 2")
-    Optional<ChatRoom> findChatRoomByMemberIds(@Param("myMemberId") Long myMemberId,
+            "WHERE m1.member.id = :myMemberId " +
+            "AND m2.member.id = :otherMemberId " +
+            "AND r.status = :status ")
+    List<ChatRoom> findChatRoomByMemberIds(@Param("myMemberId") Long myMemberId,
                                                @Param("otherMemberId") Long otherMemberId,
                                                @Param("status") ChatRoomStatus status);
 
