810cd1965a3b3b3e7edf4ea66799584d9a6d9434
조현아 <monicajo074@gmail.com>
2024-08-14 10:02:04 +0900

    Feat: [BE] 채팅에서 양방향 하트 시 주선자 목록 제공

--- Backend/zinzin/src/main/java/com/fanclub/zinzin/domain/chatting/dto/HeartToggleDto.java
+++ Backend/zinzin/src/main/java/com/fanclub/zinzin/domain/chatting/dto/HeartToggleDto.java
@@ -1,13 +1,17 @@
 package com.fanclub.zinzin.domain.chatting.dto;
 
 
+import com.fanclub.zinzin.domain.person.dto.Mate;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
 public class HeartToggleDto {
     private boolean isHeart;
+    private List<Mate> mates;
 
     public boolean getIsHeart() {
         return isHeart;
