19b9c1a6e3355de6742917103e6cbba5193a896d
홍창기 <hckhong@naver.com>
2024-08-14 11:22:06 +0900

    Fix: [BE] 파일 용량 초과 관련 ErrorCode 정의 및 Handler에 추가

--- Backend/zinzin/src/main/java/com/fanclub/zinzin/global/error/code/FileErrorCode.java
+++ Backend/zinzin/src/main/java/com/fanclub/zinzin/global/error/code/FileErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum FileErrorCode implements ErrorCode {
     FILE_UPLOAD_CONVERT_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "L001", "파일 업로드 중 오류가 발생했습니다."),
     FILE_UPLOAD_CREATE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "L002", "파일 업로드 중 오류가 발생했습니다."),
-    FILE_UPLOAD_WRITE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "L003", "파일 업로드 중 오류가 발생했습니다.");
+    FILE_UPLOAD_WRITE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "L003", "파일 업로드 중 오류가 발생했습니다."),
+    FILE_MAX_UPLOAD_SIZE_EXCEEDED(HttpStatus.PAYLOAD_TOO_LARGE, "L004", "파일의 용량이 업로드 가능한 최대 용량을 초과했습니다.");
 
     private final HttpStatus status;
     private final String code;
